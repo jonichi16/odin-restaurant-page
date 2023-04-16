892e126a89d6b8c926b006d9e6abd2784268a7dd
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-17 01:16:39 +0800

    style: Add font lobster to welcome

--- src/javascript/home.js
+++ src/javascript/home.js
@@ -1,20 +1,21 @@
 const home = (() => {
   const homeContainer = document.createElement('div');
   const title = document.createElement('h1');
+  const resto = document.createElement('span');
   const description = document.createElement('p');
-  const container = document.createElement('div');
 
   homeContainer.classList.add('home');
   title.classList.add('title');
+  resto.classList.add('resto');
   description.classList.add('description');
-  container.classList.add('container');
 
-  title.textContent = 'Welcome to Atomic Eatery!';
+  title.textContent = 'Welcome to ';
+  resto.textContent = 'Atomic Eatery';
   description.textContent =
     'Located in the heart of downtown, Atomic Eatery is a retro-themed restaurant that specializes in classic American comfort food. Our menu is packed with mouth-watering dishes that will transport you back to the 1950s, including burgers, buffalo wings, and homemade pies.';
 
-  container.append(title, description);
-  homeContainer.appendChild(container);
+  title.appendChild(resto);
+  homeContainer.append(title, description);
 
   const getPage = () => homeContainer;
 
