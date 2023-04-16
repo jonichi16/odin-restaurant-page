bb8166647c30383b96fdeefc263fbf74f0524f92
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-16 21:18:02 +0800

    feat: Add container in home

--- src/javascript/home.js
+++ src/javascript/home.js
@@ -2,16 +2,19 @@ const home = (() => {
   const homeContainer = document.createElement('div');
   const title = document.createElement('h1');
   const description = document.createElement('p');
+  const container = document.createElement('div');
 
   homeContainer.classList.add('home');
   title.classList.add('title');
   description.classList.add('description');
+  container.classList.add('container');
 
   title.textContent = 'Welcome to Atomic Eatery!';
   description.textContent =
     'Located in the heart of downtown, Atomic Eatery is a retro-themed restaurant that specializes in classic American comfort food. Our menu is packed with mouth-watering dishes that will transport you back to the 1950s, including burgers, buffalo wings, and homemade pies.';
 
-  homeContainer.append(title, description);
+  container.append(title, description);
+  homeContainer.appendChild(container);
 
   const getPage = () => homeContainer;
 
