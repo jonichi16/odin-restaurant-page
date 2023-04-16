84e672024e7a9b65c964f5c697bd5b805e3f066f
Jhonel Villanueva <vvjhonel@gmail.com>
2023-04-16 16:45:13 +0800

    feat: Add hide and show nav menu

--- src/javascript/header.js
+++ src/javascript/header.js
@@ -10,11 +10,15 @@ const header = (() => {
   const nav = document.createElement('nav');
   const ul = document.createElement('ul');
   const logo = document.createElement('div');
+  const navToggle = document.createElement('div');
+  const hamburgerMenu = document.createElement('div');
 
-  headerContainer.classList.add('header');
+  headerContainer.classList.add('header', 'relative');
   nav.classList.add('nav');
-  ul.classList.add('navMenu');
+  ul.classList.add('nav-menu', 'hidden');
   logo.classList.add('logo');
+  navToggle.classList.add('nav-toggle');
+  hamburgerMenu.classList.add('hamburger');
 
   logo.textContent = 'Atomic Eatery';
 
@@ -22,7 +26,7 @@ const header = (() => {
     const li = document.createElement('li');
     const a = document.createElement('a');
 
-    a.classList.add('navItem');
+    a.classList.add('nav-item');
 
     a.href = menu.url;
     a.textContent = menu.label;
@@ -32,14 +36,26 @@ const header = (() => {
     ul.appendChild(li);
   });
 
-  nav.appendChild(logo);
-  nav.appendChild(ul);
+  navToggle.addEventListener('click', () => {
+    ul.classList.toggle('hidden');
+  });
+
+  document.addEventListener('click', (e) => {
+    if (!navToggle.contains(e.target)) {
+      if (!ul.contains(e.target)) {
+        ul.classList.add('hidden');
+      }
+    }
+  });
+
+  navToggle.appendChild(hamburgerMenu);
+  nav.append(logo, ul, navToggle);
   headerContainer.appendChild(nav);
 
   const getHeader = () => headerContainer;
 
   const getNavMenus = () => {
-    const navMenus = document.querySelectorAll('.navItem');
+    const navMenus = document.querySelectorAll('.nav-item');
     return navMenus;
   };
 
